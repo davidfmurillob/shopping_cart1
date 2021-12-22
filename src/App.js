31a8f8b59b9670e1f134b8ca14b243f24eeda3a9
davidfmurillob <davidfmurillob@gmail.com>
2021-12-22 11:33:07 -0500

    Second commit shopping cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Producto from "./components/Producto";
+import Carrito from "./components/Carrito";
 
 function App() {
+  //useState
+  const [productos, setProductos] = useState([
+    { id: 1, nombre: "Camisa Roja", precio: 50 },
+    { id: 2, nombre: "Camisa Negra", precio: 60 },
+    { id: 3, nombre: "Camisa verde", precio: 40 },
+    { id: 4, nombre: "Camisa Azul", precio: 30 },
+  ]);
+
+  const [carrito, agregarProducto] = useState([]);
+
+  //Obtener la fecha
+  const fecha = new Date().getFullYear();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header title="El jeder jsjs" fecha={fecha} namber={300} />
+      <h1>Lista de productos mi papá</h1>
+      {productos.map((producto) => (
+        <Producto
+          key={producto.id}
+          producto={producto}
+          productos={productos}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        />
+      ))}
+
+      <Carrito carrito={carrito} agregarProducto={agregarProducto} />
+
+      <Footer />
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header({ title, namber }) {
+  return (
+    <h1>
+      {title} y el namber es: {namber}
+    </h1>
+  );
+}
+
+export default Header;
